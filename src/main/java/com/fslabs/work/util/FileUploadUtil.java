79423a0d3f4c77c6f343d636c33df2665c5bc9e0
BRPersonal <balajirengan@gmail.com>
2024-07-26 16:20:32 +0530

    Added getFileAsResource() method

--- src/main/java/com/fslabs/work/util/FileUploadUtil.java
+++ src/main/java/com/fslabs/work/util/FileUploadUtil.java
@@ -9,6 +9,12 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+
 
 @Slf4j
 public class FileUploadUtil
@@ -32,7 +38,24 @@ public class FileUploadUtil
         }
         catch (IOException ioe)
         {
-            throw new IOException("Could not save image file: " + filename, ioe);
+            throw new IOException("Could not save file: " + filename, ioe);
+        }
+    }
+
+    public static Resource getFileAsResource(String downloadDir, String fileName) throws IOException
+    {
+        Path path = Paths.get(downloadDir);
+
+        List<Path> foundFiles = Files.list(path).filter(file -> file.getFileName().toString().startsWith(fileName))
+                .collect(Collectors.toList());
+
+        if (!foundFiles.isEmpty())
+        {
+            return new UrlResource(foundFiles.get(0).toUri());
+        }
+        else
+        {
+            throw new RuntimeException(String.format("%s not found", fileName));
         }
     }
 }
